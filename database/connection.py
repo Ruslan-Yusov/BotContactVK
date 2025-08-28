7bce7c8f7e2f3d29133692e866e0759a486c60c3
RuslanYusov <yusov.ruslan@yandex.ru>
2025-08-28 20:04:16 +0300

    init commit

--- database/connection.py
+++ database/connection.py
@@ -1,10 +1,21 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:admin@localhost:5432/vkbot"
+load_dotenv("config.env")
 
-engine = create_engine(DATABASE_URL)
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+
+if not all([db_user, db_password, db_host, db_port, db_name]):
+    raise ValueError("One or more database parameters are not set in environment variables")
+
+engine = create_engine(f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- database/models.py
+++ database/models.py
@@ -49,3 +49,14 @@ class FavoriteUser(Base):
     profile_url = Column(Text, nullable=True)
 
     user = relationship("User", back_populates="favorites")
+
+class Blacklist(Base):
+    __tablename__ = 'blacklist'
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"), nullable=False)
+    vk_id = Column(BigInteger, nullable=False)
+    name = Column(String(255), nullable=True)
+    profile_url = Column(Text, nullable=True)
+
+    user = relationship("User", back_populates="blacklist")
